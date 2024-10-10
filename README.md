156da01f7edb3eceb87979a5b8ea8d4c5b7d813d
KOMAD-bit <63553049+KOMAD-bit@users.noreply.github.com>
2024-10-10 11:57:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# hello home assistant1
+This integration is for home assistant for user who wants start developing own integrations.
---
+++ custom_components/hello_home_assistant1/const.py
@@ -0,0 +1,4 @@
+"""Constants for the Hello Home Assistant integration 1."""
+
+DOMAIN = "hello_home_assistant1"
+CONF_NAME = "Configname"
